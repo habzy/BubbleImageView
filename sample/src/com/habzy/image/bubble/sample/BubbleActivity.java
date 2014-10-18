ad7deffbc7b71c0dcd1cd780089fd6277c5dd3f2
Habzy Huang <habzyhs@gmail.com>
2014-10-18 17:06:44 +0800

    Update icons

--- sample/src/com/habzy/image/bubble/sample/BubbleActivity.java
+++ sample/src/com/habzy/image/bubble/sample/BubbleActivity.java
@@ -11,7 +11,7 @@ public class BubbleActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.bubble_activity);
+        setContentView(R.layout.activity_bubble);
     }
 
 
